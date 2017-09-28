9f9b6975d479e9b8f96def23e95f4f95b46709d4
GeorgeTuz <mih_petro@mail.ru>
2017-09-28 11:15:52 +0200

    GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,31 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+		this.array = [];
+		this.startNum = null;
+		this.endNum = null;
+		this.midleNum = null;
+	}
 
     setRange(min, max) {
-
+		for (var i=min; i<max; i++) {
+			this.array.push(i);
+		}
+		
+		this.startNum = 0;
+		this.endNum = this.array.length;
     }
 
     guess() {
-
+		this.midleNum = Math.ceil((this.startNum + this.endNum)/2);
+		return this.array[this.midleNum];
     }
 
     lower() {
-
+		this.endNum = this.midleNum;
     }
 
     greater() {
-
+		this.startNum = this.midleNum;
     }
 }
 
